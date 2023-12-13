e98fffca5b338010aa5e1fb7e220dbe9fe10c16b
setiawan2023 <151593560+setiawan2023@users.noreply.github.com>
2023-12-13 16:03:28 +0700

    Add files via upload
    Hasil Ujian Praktek  AGUNG,ALYA,HASBI

---
+++ luas segitiga.cpp
@@ -0,0 +1,31 @@
+#include <cstdlib>
+#include <iostream>
+
+using namespace std;
+
+float fluassegitiga(int a,int t){
+    return (a*t)/2;
+    
+}
+int main(){
+    int a,t,p,l,s,pilih;
+    float hasil;
+    cout<<"========================="<<endl;
+    cout<<"# Program mencari Luas #"<<endl;
+    cout<<"# Program Di Buat By Hasbi #"<<endl;
+    cout<<"========================="<<endl;
+    cout<<"Pilihlah program Yang di inginkan"<<endl;
+    cout<<"Menghitung Luas segitiga"<<endl;
+}
+if (pilih ==){
+  cout<<"menghitung luas Segitiga"<<endl;
+        cout<<"masukkan nilai alas : ";
+        cin>>a;
+        cout<<"masukkan nilai tinggi: ";
+        cin>>t;
+        hasil = fluassegitiga(a,t);
+        cout<<"luassegitiga : "<<hasil<<endl;
+    }else{
+  system("PAUSE");
+  return 0;
+}
